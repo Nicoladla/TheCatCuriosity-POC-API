79d8ec312a2ec90e410676ecab92922645a3475e
Nicolas Araújo <nicolasaraujo@gmail.com>
2023-01-23 17:30:47 -0300

    feat: adicionado os tipos aos schema de validação

--- src/schema/classificationsSchema.ts
+++ src/schema/classificationsSchema.ts
@@ -1,6 +1,7 @@
 import joi from "joi";
+import { ClassificationInsert } from "../protocols/ClassificationProtocol.js";
 
-const classificationSchema = joi.object({
+const classificationSchema = joi.object<ClassificationInsert>({
   name: joi.string().min(3).max(20).required(),
 });
 
--- src/schema/curiositiesSchema.ts
+++ src/schema/curiositiesSchema.ts
@@ -1,13 +1,17 @@
 import joi from "joi";
+import {
+  CuriositiesInsert,
+  CuriositiesUpdate,
+} from "../protocols/curiositiesProtocol.js";
 
-export const curiositySchema = joi.object({
+export const curiositySchema = joi.object<CuriositiesInsert>({
   author: joi.string().min(3).required(),
   title: joi.string().min(4).required(),
   description: joi.string().min(15).required(),
   classificationId: joi.number().min(1).required(),
 });
 
-export const curiositiesUpdateSchema = joi.object({
+export const curiositiesUpdateSchema = joi.object<CuriositiesUpdate>({
   title: joi.string().min(4).required(),
   description: joi.string().min(15).required(),
 });
